dd1c0aa30c584592bd093ebfe27931324396a96f
VeronikaLia-1 <veronikalian@hotmail.com>
2024-10-02 19:05:31 +0200

    Create Oving_6_les_inn_data.py
    Har prøvd meg litt fram med kode som leser inn data fra den ene fila, og plotter dette, en for temperatur og en for trykk

---
+++ Oving_6_les_inn_data.py
@@ -0,0 +1,48 @@
+# -*- coding: utf-8 -*-
+
+import csv
+from datetime import datetime
+import matplotlib.pyplot as plt
+
+def les_data(filnavn):
+
+    lokal_tidspunkt = []
+    lokal_temperatur = []
+    lokal_trykk = []
+    
+    
+    with open(filnavn, newline='') as csvfile:
+        reader = csv.reader(csvfile, delimiter=';')
+        next(reader)
+        for rad in reader:
+            if len(rad) == 5: 
+                dato_tid = rad[0]
+                absolutt_trykk = rad[3].replace(",", ".")
+                temperatur = rad[4].replace(",", ".")
+                
+                try: 
+                    dato_tid_objekt = datetime.strptime(dato_tid,'%m.%d.%Y %H:%M')
+                    lokal_tidspunkt.append(dato_tid_objekt)
+                    lokal_trykk.append(float(absolutt_trykk))
+                    lokal_temperatur.append(float(temperatur))
+                except ValueError:
+                    continue
+  
+    return lokal_tidspunkt, lokal_trykk, lokal_temperatur
+        
+filnavn = "trykk_og_temperaturlogg_rune_time.csv.txt"
+lokal_tidspunkt, lokal_trykk, lokal_temperatur = les_data(filnavn)
+
+plt.subplot(2, 1, 1)
+plt.plot(lokal_tidspunkt, lokal_temperatur, marker ="o", color="blue", label="Temperatur (Celcius)")
+plt.grid()
+plt.legend()
+
+plt.subplot(2, 1, 2)
+plt.plot(lokal_tidspunkt, lokal_trykk, marker ="o", color ="red", label="Trykk")
+plt.grid()
+plt.legend()
+
+plt.tight_layout()
+plt.show()
+
